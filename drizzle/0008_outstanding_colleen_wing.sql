a01279c634977a1bde6fdbe61c17e6384f877ef6
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2025-09-11 19:02:06 -0300

    fix: jwk

---
+++ drizzle/0008_outstanding_colleen_wing.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "verification" (
+	"id" text PRIMARY KEY NOT NULL,
+	"identifier" text NOT NULL,
+	"value" text NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"created_at" timestamp,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+DROP TABLE "jwks" CASCADE;
--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { bearer, jwt } from "better-auth/plugins";
+import { bearer } from "better-auth/plugins";
 import { db } from "./db/client";
 
 console.log(process.env.CLIENT_ORIGIN, process.env.BETTER_AUTH_URL);
 export const auth = betterAuth({
-  plugins: [bearer(), jwt()],
+  plugins: [bearer()],
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
