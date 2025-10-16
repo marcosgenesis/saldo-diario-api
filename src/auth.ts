554abc6d3d0c346d8fb10aa8aaffb1e9fced0238
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2025-10-16 06:57:15 -0300

    feat: expo

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,17 @@
+import { expo } from "@better-auth/expo";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { bearer } from "better-auth/plugins";
 import { db } from "./db/client";
 
 export const auth = betterAuth({
-  plugins: [bearer()],
+  plugins: [bearer(), expo()],
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
   trustedOrigins: [
     process.env.CLIENT_ORIGIN || "https://app.saldodiario.com.br",
+    "saldodiario://",
   ],
   emailAndPassword: {
     enabled: true,
